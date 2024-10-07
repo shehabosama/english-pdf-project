78f0b6e48d7b3b0c1e26c08f3c7afd37720de9d6
ShehabOsama <shehab.osama@squadio.com>
2024-10-07 12:25:18 +0300

    add signed script to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.english.pdf"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
