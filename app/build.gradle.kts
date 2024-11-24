104785acef893b6651e75c29a3195a1aeb299d9a
ShehabOsama <shehab.osama@squadio.com>
2024-11-24 23:10:25 +0200

    increase the version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -57,4 +57,16 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+//    implementation ("org.koin:koin-android:2.0.1")
+//    implementation ("org.koin:koin-androidx-scope:2.0.1")
+//    implementation ("org.koin:koin-androidx-viewmodel:2.0.1")
+//    implementation ("org.koin:koin-androidx-ext:2.0.1")
+//    implementation ("org.koin:koin-java:2.0.1")
+//    implementation ("org.koin:koin-test:2.0.1")
+
+    // implementation "io.insert-koin:koin-androidx-scope:3.5.0"
+
+    // Keep only the necessary dependencies
+    implementation("io.insert-koin:koin-android:3.5.0")
+
 }
