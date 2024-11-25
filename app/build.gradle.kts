3be11b4ab3f47e4e3c2d4c42fc0ec11b117c942b
ShehabOsama <shehab.osama@squadio.com>
2024-11-25 06:02:51 +0200

    increase the version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.english.pdf"
         minSdk = 28
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
